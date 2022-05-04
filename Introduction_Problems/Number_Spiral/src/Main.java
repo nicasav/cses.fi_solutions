4e33fc429ac818e91dca77ee88b085dda2ba478e
Nidai Çağrı Savran <nsavran19@ku.edu.tr>
2022-05-04 17:46:08 +0300

    Increasing Array Implemented

--- Introduction_Problems/Number_Spiral/src/Main.java
+++ Introduction_Problems/Number_Spiral/src/Main.java
@@ -15,21 +15,21 @@ public class Main {
     }
   }
   public static void printval(long x, long y){
-      long print;
-      if(y > x) {
-        if (y % 2 == 1){
-          print = y * y - (x - 1);
-        } else {
-          print = (y - 1) * (y - 1) + 1 + (x - 1);
-        }
-      } else{
-        if (x % 2 == 1){
-          print = x * x - (x - 1) - (x - y);
-        } else {
-          print = (x-1) * (x-1) + 1 + (x - 1) + (x - y);
-        }
+    long print;
+    if(y > x) {
+      if (y % 2 == 1){
+        print = y * y - (x - 1);
+      } else {
+        print = (y - 1) * (y - 1) + 1 + (x - 1);
       }
-      System.out.println(print);
+    } else{
+      if (x % 2 == 1){
+        print = x * x - (x - 1) - (x - y);
+      } else {
+        print = (x-1) * (x-1) + 1 + (x - 1) + (x - y);
+      }
+    }
+    System.out.println(print);
 
   }
 
